f98dc870510507fcf8232007f12c77f9630ee5e6
ZhurkinN <zhurkin236@gmail.com>
2023-03-11 02:23:01 +0300

    intial commit

---
+++ src/main/java/ru/zhurkin/sbercinema/SberCinemaApplication.java
@@ -0,0 +1,13 @@
+package ru.zhurkin.sbercinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zhurkin/sbercinema/SberCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhurkin.sbercinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
